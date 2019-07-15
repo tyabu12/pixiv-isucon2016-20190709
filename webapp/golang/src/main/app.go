5b248b4085ab70bd72eff4e4c89bca0577518258
Yabu Tomohito <tyabu1212@gmail.com>
2019-07-15 10:26:08 +0900

    ログ出力関係をストップ

--- webapp/golang/src/main/app.go
+++ webapp/golang/src/main/app.go
@@ -1048,9 +1048,9 @@ func postAdminBanned(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	// go func() {
+	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
+	// }()
 
 	host := os.Getenv("ISUCONP_DB_HOST")
 	if host == "" {
